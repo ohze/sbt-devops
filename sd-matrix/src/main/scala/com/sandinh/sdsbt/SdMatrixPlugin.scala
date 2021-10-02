5dce0ff55b245e4c76152bdcb2d937104398fb0b
Bùi Việt Thành <thanhbv@sandinh.net>
2021-10-03 01:20:31 +0700

    sd-matrix: Add akkaAxis, playAxis  & remove akkaVersion, playVersion keys

--- sd-matrix/src/main/scala/com/sandinh/sdsbt/SdMatrixPlugin.scala
+++ sd-matrix/src/main/scala/com/sandinh/sdsbt/SdMatrixPlugin.scala
@@ -13,31 +13,70 @@ object SdMatrixPlugin extends AutoPlugin {
   override def requires: Plugins = ProjectMatrixPlugin
 
   object autoImport {
-    lazy val akkaVersion = settingKey[String]("akkaVersion")
-    lazy val playVersion = settingKey[String]("playVersion")
+
+    /** Usage example: {{{
+      *   libraryDependencies ++= Seq(
+      *     "com.sandinh" %% s"mqtt-proto${akkaAxis.value.suffix}" % "1.10.1",
+      *     "com.typesafe.akka" %% "akka-actor" % akkaAxis.value.version cross CrossVersion.for3Use2_13,
+      *   )
+      *   // for multiple akka modules, you should use `akka` method:
+      *   libraryDependencies ++= akka("actor", "testkit" -> Test).value
+      * }}}
+      */
+    lazy val akkaAxis = settingKey[LibAxis]("Current akka LibAxis")
+
+    /** Usage example: {{{
+      *   libraryDependencies ++= Seq(
+      *     "com.sandinh" %% "couchbase-play${playAxis.value.suffix}" % "9.1.0",
+      *     "com.typesafe.play" %% "play-cache" % playAxis.value.version,
+      *   )
+      *   // for multiple play modules, you should use `play` method:
+      *   libraryDependencies ++= play("play", "cache", "jdbc" -> Test).value
+      * }}}
+      */
+    lazy val playAxis = settingKey[LibAxis]("Current play LibAxis")
 
     val (akka25, akka26) = (LibAxis("2.5.32"), LibAxis("2.6.16", ""))
 
     val (play26, play27, play28) =
       (LibAxis("2.6.25"), LibAxis("2.7.9"), LibAxis("2.8.8", ""))
 
-    // TODO remove `for3Use2_13` when this issue is fixed: https://github.com/akka/akka/issues/30243
-    def akka(modules: NameAnConfig*): Initialize[Seq[ModuleID]] = akkaVersion {
-      v =>
+    /** Usage example: {{{
+      *   libraryDependencies ++= akka("actor", "testkit" -> Test).value
+      *   // same as:
+      *   libraryDependencies ++= Seq(
+      *     "com.typesafe.akka" %% "akka-actor" % akkaAxis.value.version cross CrossVersion.for3Use2_13,
+      *     "com.typesafe.akka" %% "akka-testkit" % akkaAxis.value.version % Test cross CrossVersion.for3Use2_13,
+      *   )
+      * }}}
+      */
+    def akka(modules: NameAnConfig*): Initialize[Seq[ModuleID]] = akkaAxis {
+      a =>
         modules.map { case NameAnConfig(m, c) =>
-          "com.typesafe.akka" %% s"akka-$m" % v withConfigurations c cross CrossVersion.for3Use2_13
+          // TODO remove `for3Use2_13` when this issue is fixed: https://github.com/akka/akka/issues/30243
+          "com.typesafe.akka" %% s"akka-$m" % a.version withConfigurations c cross CrossVersion.for3Use2_13
         }
     }
 
-    def play(modules: NameAnConfig*): Initialize[Seq[ModuleID]] = playVersion {
-      v =>
+    /** Usage example: {{{
+      *   libraryDependencies ++= play("play", "cache", "jdbc" -> Test).value
+      *   // same as:
+      *   libraryDependencies ++= Seq(
+      *     "com.typesafe.play" %% "play" % playAxis.value.version,
+      *     "com.typesafe.play" %% "play-cache" % playAxis.value.version,
+      *     "com.typesafe.play" %% "play-jdbc" % playAxis.value.version % Test,
+      *   )
+      * }}}
+      */
+    def play(modules: NameAnConfig*): Initialize[Seq[ModuleID]] = playAxis {
+      p =>
         modules.map {
           case NameAnConfig("" | "play", c) =>
-            "com.typesafe.play" %% "play" % v withConfigurations c
+            "com.typesafe.play" %% "play" % p.version withConfigurations c
           case NameAnConfig(m @ "json", _) =>
             sys.error(s"play-$m don't have save version as play")
           case NameAnConfig(m, c) =>
-            "com.typesafe.play" %% s"play-$m" % v withConfigurations c
+            "com.typesafe.play" %% s"play-$m" % p.version withConfigurations c
         }
     }
 
@@ -62,32 +101,30 @@ object SdMatrixPlugin extends AutoPlugin {
           lib: LibAxis,
           scalaVersions: Seq[String],
           process: Project => Project = identity,
-      ): ProjectMatrix = p
-        .customRow(
+      ): ProjectMatrix =
+        libAxis(
+          lib,
           scalaVersions,
-          axisValues = Seq(lib, jvm),
-          p =>
-            process(
-              p.settings(
-                moduleName := moduleName.value + lib.suffix,
-                akkaVersion := lib.version,
-              )
-            )
+          p => process(p.settings(autoImport.akkaAxis := lib))
         )
 
       def playAxis(
           lib: LibAxis,
           scalaVersions: Seq[String],
           process: Project => Project = identity,
-      ): ProjectMatrix = p
-        .customRow(
+      ): ProjectMatrix =
+        libAxis(
+          lib,
           scalaVersions,
-          axisValues = Seq(lib, jvm),
           p =>
             process(
               p.settings(
-                moduleName := moduleName.value + lib.suffix,
-                playVersion := lib.version,
+                autoImport.playAxis := lib,
+                autoImport.akkaAxis := (lib match {
+                  case `play26` | `play27` => akka25
+                  case `play28`            => akka26
+                  case _                   => sys.error(s"Not support $lib")
+                }),
               )
             )
         )
