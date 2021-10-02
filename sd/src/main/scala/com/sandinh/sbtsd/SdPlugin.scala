9f5da6c5e31c1b5d95079dbe3825f4d9255e31b1
Bùi Việt Thành <thanhbv@sandinh.net>
2021-10-02 16:58:33 +0700

    SdPlugin: Don't auto add scala-collection-compat

--- sd/src/main/scala/com/sandinh/sbtsd/SdPlugin.scala
+++ sd/src/main/scala/com/sandinh/sbtsd/SdPlugin.scala
@@ -4,7 +4,6 @@ import sbt.*
 import sbt.Keys.*
 import com.sandinh.devops.DevopsPlugin
 import DevopsPlugin.autoImport.devopsNexusHost
-
 import scala.collection.immutable.Seq
 import scala.collection.mutable.ListBuffer
 
@@ -16,8 +15,17 @@ object SdPlugin extends AutoPlugin {
     val (scala211, scala212, scala213, scala3) =
       ("2.11.12", "2.12.15", "2.13.6", "3.0.2")
 
-    val skipPublish: Seq[Setting[?]] = SdPlugin.skipPublish
+    val skipPublish: Seq[Setting[?]] = Seq(
+      publish / skip := true,
+      publishLocal / skip := true,
+    )
+
+    val scalaColCompat: ModuleID =
+      "org.scala-lang.modules" %% "scala-collection-compat" % "2.5.0"
   }
+
+  def silencer(m: String): ModuleID =
+    "com.github.ghik" % s"silencer-$m" % "1.7.6" cross CrossVersion.full
 
   override def globalSettings: Seq[Setting[?]] = Seq(
     devopsNexusHost := "repo.bennuoc.com",
@@ -29,18 +37,8 @@ object SdPlugin extends AutoPlugin {
 
   override def projectSettings: Seq[Setting[?]] = Seq(
     scalacOptions ++= sdScalacOptions(scalaVersion.value),
-    Compile / scalacOptions ++= (scalaBinaryVersion.value match {
-      // TODO enable -Xfatal-warnings when this is RELEASED in scala3:
-      // https://github.com/lampepfl/dotty/pull/12857
-      case "3" => Nil
-      case _   => Seq("-Xfatal-warnings")
-    }),
-    libraryDependencies ++= silencerAndColCompat(scalaBinaryVersion.value),
-  )
-
-  val skipPublish: Seq[Setting[?]] = Seq(
-    publish / skip := true,
-    publishLocal / skip := true,
+    Compile / scalacOptions ++= fatalWarnings(scalaBinaryVersion.value),
+    libraryDependencies ++= silencerDeps(scalaBinaryVersion.value),
   )
 
   /** @param scalaVersion scala version. Ex 2.11.12, 3.1.0-RC2,..
@@ -63,14 +61,17 @@ object SdPlugin extends AutoPlugin {
     opts.result()
   }
 
-  def silencerAndColCompat(scalaBinVersion: String): Seq[ModuleID] = {
-    val silencers = // https://github.com/ghik/silencer
-      if (scalaBinVersion != "2.11") Nil
-      else Seq(compilerPlugin(silencer("plugin")), silencer("lib") % Provided)
-    silencers ++
-      Seq("org.scala-lang.modules" %% "scala-collection-compat" % "2.5.0")
+  // https://github.com/ghik/silencer
+  def silencerDeps(scalaBinVersion: String): Seq[ModuleID] = {
+    if (scalaBinVersion != "2.11") Nil
+    else Seq(compilerPlugin(silencer("plugin")), silencer("lib") % Provided)
   }
 
-  private def silencer(m: String) =
-    "com.github.ghik" % s"silencer-$m" % "1.7.6" cross CrossVersion.full
+  def fatalWarnings(scalaBinVersion: String): Seq[String] =
+    (scalaBinVersion match {
+      // TODO enable -Xfatal-warnings when this is RELEASED in scala3:
+      // https://github.com/lampepfl/dotty/pull/12857
+      case "3" => Nil
+      case _   => Seq("-Xfatal-warnings")
+    })
 }
