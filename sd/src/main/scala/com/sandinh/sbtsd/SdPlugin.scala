bfffc4194aede9fb15c454fc34bb05fa896bd221
Bui Viet Thanh <thanhbv@sandinh.net>
2023-06-05 21:43:40 +0700

    Update specs2 5.2.0 for scala 3 & 4.20.0 for scala 2

--- sd/src/main/scala/com/sandinh/sbtsd/SdPlugin.scala
+++ sd/src/main/scala/com/sandinh/sbtsd/SdPlugin.scala
@@ -49,8 +49,8 @@ object SdPlugin extends AutoPlugin {
     def specs2(modules: String*): Initialize[CSeq[ModuleID]] = Def.setting {
       val specs2Version = scalaBinaryVersion.value match {
         case "2.11" => "4.10.6"
-        case "3"    => "5.0.0-RC-22"
-        case _      => "4.13.1"
+        case "3"    => "5.2.0"
+        case _      => "4.20.0"
       }
       modules.map { m => "org.specs2" %% s"specs2$m" % specs2Version % Test }
     }
